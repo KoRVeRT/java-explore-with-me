f31bd328aaaca554ec2b0a2261dd35ff140ff585
Masharov Dmitry <staker@yandex.ru>
2023-06-22 15:30:41 +0300

    исправил стиль кода в StatsService

--- stats-service/stats-server/src/main/java/ru/practicum/ewm/server/stats/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/server/stats/service/StatsService.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsService {
-    void saveHit(HitDto dto) ;
+    void saveHit(HitDto dto);
 
     List<ViewDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 }
